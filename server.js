84d00a8fe41055d22f84f7fd8e53c7ea20f885ba
Aleksandr Shatskikh <work@errand.ru>
2022-05-16 16:46:33 +0300

    feat: delete working

--- server.js
+++ server.js
@@ -20,16 +20,10 @@ const router = new Router();
 
 function fortune(ctx, body = null, status = 200) {
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (Math.random() > 0.25) {
-                ctx.response.status = status;
-                ctx.response.body = body;
-                resolve();
-                return;
-            }
-
-            reject(new Error('Something bad happened'));
-        }, 3 * 1000);
+        ctx.response.status = status;
+        ctx.response.body = body;
+        resolve();
+        return;
     })
 }
 
